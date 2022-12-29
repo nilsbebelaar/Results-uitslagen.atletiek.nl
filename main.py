c6ba735eb305f8e562d3d0ad97db3b899a5c688d
Nils Bebelaar <nils@bebelaar.net>
2022-12-30 00:06:00 +0100

    Restructure to one 'competition' object

--- main.py
+++ main.py
@@ -4,26 +4,30 @@ from bs4 import BeautifulSoup
 import json
 from categories import category_to_gender
 
+COMP_TYPE = 'indoor'
 COMP_ID = 8702
 BASE_URL = 'https://uitslagen.atletiek.nl'
 
 
 def main():
-    resultlists = get_resultlists(COMP_ID)
-    competitors = get_competitors(COMP_ID)
-    competitors = find_results(resultlists, competitors)
+    competition = {'id': COMP_ID, 'type': COMP_TYPE}
+    competition = get_resultlists(competition)
+    competition = get_registrations(competition)
+    competition = find_results(competition)
 
     json_string = json.dumps(competitors)
     pyperclip.copy(json_string)  # Copy the json to clipboard
     print(json_string)
 
 
-def find_results(resultlists, competitors):
-    competitors_list = []
-    for resultlist in resultlists:
+
+
+def find_results(competition):
+    competition['competitors'] = []
+    for resultlist in competition['resultlists']:
         for result in resultlist['results']:
             # For every result, loop through all competitors
-            for competitor in competitors:
+            for competitor in competition['registrations']:
                 # If a competitor's bib-number exists in the results, append the result to the competitor['results'] list
                 if result['bib'] == competitor['bib']:
                     competitor['category'] = result['category']
@@ -33,45 +37,49 @@ def find_results(resultlists, competitors):
                         'result': result['result'],
                         'url': resultlist['url_result']
                     })
-                    competitors_list.append(dict(sorted(competitor.items())))
-    return competitors_list
+                    competition['competitors'].append(competitor)
+    return competition
 
 
-def get_competitors(competition_id):
-    url = BASE_URL + '/Competitions/Competitoroverview/' + str(competition_id)
+def get_registrations(competition):
+    url = BASE_URL + '/Competitions/Competitoroverview/' + str(competition['id'])
     session = requests.session()
 
     soup = BeautifulSoup(session.get(url).text, 'html.parser')
 
-    competitors = []
     for line in soup.find('div', {'class': 'blocktable'}).find_all('div', {'class': 'entryline'}):
+    competition['registrations'] = []
         competitor = {}
         competitor['bib'] = line.find('div', {'class': 'col-1'}).find('div', {'class': 'firstline'}).text.strip()
         competitor['name'] = line.find('div', {'class': 'col-2'}).find('div', {'class': 'firstline'}).text.strip()
         competitor['club'] = line.find('div', {'class': 'col-2'}).find('div', {'class': 'secondline'}).text.strip()
         competitor['birthyear'] = line.find('div', {'class': 'col-42p'}).find('div', {'class': 'secondline'}).text.strip()
         competitor['results'] = []
-        competitors.append(competitor)
+        competition['registrations'].append(competitor)
 
     session.close()
-    return competitors
+    return competition
 
 
-def get_resultlists(competition_id):
-    url = BASE_URL + '/Competitions/Details/' + str(competition_id)
+def get_resultlists(competition):
+    url = BASE_URL + '/Competitions/Details/' + str(competition['id'])
     session = requests.session()
 
     page_competition = BeautifulSoup(session.get(url).text, 'html.parser')
 
-    resultlists = []
 
+    competition['resultlists'] = []
+    # Find all result lists
     for block in page_competition.find_all('div', {'class': 'blockcontent'}):
         for a in block.find_all('a'):
             result_url = BASE_URL + a['href']
             register_url = result_url.replace('CurrentList', 'RegisterList')
-            resultlists.append({'url_result': result_url, 'url_registrations': register_url})
+            # Save the result url as well as the url with all registrations
+            # Registrations are needed as the catergory is not visible on the results page
+            competition['resultlists'].append({'url_result': result_url, 'url_registrations': register_url})
 
-    for resultlist in resultlists:
+    for resultlist in competition['resultlists']:
+
         page_result = BeautifulSoup(session.get(resultlist['url_result']).text, 'html.parser')
 
         # Name of resultlist is part of <div class="leftheader">
@@ -101,7 +109,7 @@ def get_resultlists(competition_id):
                     result['gender'] = category_to_gender(result['category'])
 
     session.close()
-    return resultlists
+    return competition
 
 
 # THIS NEEDS EXTRA WORK
