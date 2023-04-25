c65d41b2c4ad40676f11c16ed64bcf012a81c321
nilsbebelaar <nils@bebelaar.net>
2023-04-25 11:24:20 +0200

    Proxy Fix

--- start.py
+++ start.py
@@ -1,6 +1,8 @@
 from app import init_app
+from werkzeug.middleware.proxy_fix import ProxyFix
 
 app = init_app()
+app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_host=1) 
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
