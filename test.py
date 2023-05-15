0f67a6f96258ffd92bba3faa0cefcdd326e34a2b
nilsbebelaar <nils@bebelaar.net>
2023-05-15 16:24:22 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
-from app.main.background import get_competition_info_xml, get_all_results, cleanup_athletes
+from app.main.background import get_competition_info_xml, get_all_results, cleanup_athletes, save_to_file
 
 comp = {
-    'id': 9153,
+    'id': 9396,
     'source': 'html',
     'domain': 'uitslagen.atletiek.nl'
 }
@@ -9,3 +9,4 @@ comp = {
 get_competition_info_xml(comp)
 get_all_results(comp)
 cleanup_athletes(comp)
+save_to_file(comp)
