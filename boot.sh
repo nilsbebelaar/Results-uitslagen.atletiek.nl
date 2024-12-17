22f648e0280ab275ce85cefb3917612c2f7bf12e
Nils Bebelaar <nils@bebelaar.net>
2024-12-17 18:27:41 +0100

    Increase timeout

--- boot.sh
+++ boot.sh
@@ -2,4 +2,4 @@
 source venv/bin/activate
 # flask db upgrade
 # flask translate compile
-exec gunicorn -b :5000 --access-logfile - --error-logfile - start:app
+exec gunicorn -b :5000 --timeout 120 --access-logfile - --error-logfile - start:app
